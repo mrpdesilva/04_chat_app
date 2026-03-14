a78e81bb76f616f288d26a2e2a149f69e8a534f4
mrpdesilva <roshanipiumali8@gmail.com>
2026-03-14 13:13:30 +0530

    first part completed -backend

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,98 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: "Password and confirm password does not match" })
+        }
+
+        const user = await User.findOne({ username })
+
+        if (user) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+
+        //Hash the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        //https://avatar-placeholder.iran.liara.run/
+
+        const seed = encodeURIComponent(username.toLowerCase().replace(/\s+/g, "_"));
+        const boyProfilePic = `https://api.dicebear.com/9.x/adventurer/png?seed=${seed}&size=128`;
+        const girlProfilePic = `https://api.dicebear.com/9.x/lorelei/png?seed=${seed}&size=128`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        if (newUser) {
+
+            //JWT Token
+            generateTokenAndSetCookie(newUser._id, res)
+
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic
+            })
+
+        }else{
+            res.status(500).json({ message: "Error occcured", error })
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        generateTokenAndSetCookie(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+        
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const logout = async(req, res) => {
+    try {
+
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({ message: "Logout successfully" })
+        
+    } catch (error) {
+        console.log("Error in logout controller", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+    try {
+        
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected to Mongo DB")
+
+    } catch (error) {
+        console.log("Error in connecting to Mongo DB", error.message)
+    }
+}
+
+export default connectToMongoDB
